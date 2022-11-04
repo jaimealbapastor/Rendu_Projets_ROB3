e0b8356ca1741e5d87ff37d0619319dba824acc2
Jaime Alba <jalbapastor@gmail.com>
2022-11-04 02:22:29 +0100

    issue undefined reference

---
+++ C++/MorseCode/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "morseTranslator.h"
+using namespace std;
+
+morseTranslator morse;
+
+int main()
+{
+    
+    // char *s;
+
+    // s = morse.translate("a");
+
+    // while (*s != '\0')
+    // {
+    //     cout << *s;
+    // }
+    cout << "Le code est execute";
+    return 0;
+}
