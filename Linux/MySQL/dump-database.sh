14390dedc4eda0833f9c9beb6841679f2530b23f
Jaime Alba <jalbapastor@gmail.com>
2022-11-23 12:51:04 +0100

    update

--- Linux/MySQL/dump-database.sh
+++ Linux/MySQL/dump-database.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
 
 user="jaime"
-#password="Shitty*pw1234"
 databaseName="classicmodels"
 
+# On cree le chemin absolu du fichier .sql
 dumpPath="/home/jaime/Documents/mysql/"
-dumpPrefix="dump_${databaseName}"
+dumpName="dump_${databaseName}"
 currentDate=$(date +%Y-%m-%d-%H-%M-%S)
-dumpName="${dumpPrefix}_${currentDate}"
-dumpFile="${dumpPath}${dumpName}.sql"
+dumpFile="${dumpPath}${dumpName}_${currentDate}.sql"
 
-#logFile="/home/jaime/logfile.log"
+# Fonction pour ajouter des logs
 logFile="/var/log/dump-mysql.log"
 addLog() {
-	if [ ! -f $logFile ]; then
+	if [ ! -f $logFile ]; then		# si le fichier n'existe pas, le créer (mais il y aura un problème de permissions dans /var/log/)
 		touch $logFile
 	fi
 	#(echo "$currentDate $1") >>$logFile
-	logger -s $1 2>>$logFile
+	logger -s $1 2>>$logFile		# on utiliser logger pour le formattage du message
 }
 
 #mysqldump -u $user -p$password $databaseName > $dumpFile
-mysqldump -u $user $databaseName >$dumpFile
+mysqldump -u $user $databaseName >$dumpFile		# télécharger une image de la database
 error=$?
 
 if [ ! $error -eq 0 ]; then
 	addLog "ERROR in mysqldump ($databaseName) : $error"
-	if [ -e $dumpFile ]; then rm $dumpFile; fi
+	if [ -e $dumpFile ]; then rm $dumpFile; fi		# supprimer le fichier .sql généré si mysqldump échoue
 	exit 1
 fi
 
-bzip2 $dumpFile
+bzip2 $dumpFile		# compresser le fichier en .bz2
 error=$?
 
 if [ ! $error -eq 0 ]; then
 	addLog "ERROR in bzip2 ($databaseName) : $error"
-	if [ -e $dumpFile ]; then rm $dumpFile; fi
+	if [ -e $dumpFile ]; then rm $dumpFile; fi		# supprimer le fichier .sql généré si bzip2 échoue
 	exit 1
 fi
 
-# find $dumpPath -maxdepth 1 -name "${dumpPrefix}*"
+# Limiter à 5 fichiers les backups de la base de données
 
 n=1
-for file in $(ls -t $dumpPath$dumpPrefix*); do
+for file in $(ls -t $dumpPath$dumpName*); do
 	if [ $n -gt 5 ]; then
 		rm $file
 		addLog "Max capacity reached: removing $file"
@@ -51,3 +50,12 @@ for file in $(ls -t $dumpPath$dumpPrefix*); do
 done
 
 addLog "Succesuflly created: $databaseName backup"
+
+# Contrôle des logs
+# si la poids du fichier $logFile est supérieur au poids maximal
+tmpLog.log=""
+while [ $(wc -c $logFile | awk '{print $1}') -gt 5000 ]; do 
+	tail -n +2 $logFile > tmp.csv && mv tmp.csv books.csv
+done
+
+exit 0
