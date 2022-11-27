750ac0181a997a80297c5be4922253fb3e00e566
Jaime Alba <jalbapastor@gmail.com>
2022-11-27 14:07:48 +0100

    add comments

--- C++/MorseCode/arduino/main/morseTranslator.cpp
+++ C++/MorseCode/arduino/main/morseTranslator.cpp
@@ -1,43 +1,55 @@
 #include "morseTranslator.h"
 
-morseTranslator::morseTranslator()
-{
+/**
+ * @brief Construct a new morse Translator::morse Translator object
+ */
+morseTranslator::morseTranslator() {
 }
 
-morseTranslator::~morseTranslator()
-{
+/**
+ * @brief Destroy the morse Translator::morse Translator object
+ */
+morseTranslator::~morseTranslator() {
 }
 
-const char *morseTranslator::getMorse(char c)
-{
+/**
+ * @brief Get the code morse of a single character
+ * 
+ * @param c : the char to translate
+ * @return const char*
+ */
+const char *morseTranslator::getMorse(char c) {
   uint8_t i = 0;
-  while (i < this->nbOfChars)
-  {
+  while (i < this->nbOfChars) {
     if (this->characters[i] == c) return this->morseCode[i];
     i++;
   }
   return this->nullMorse;
 }
 
-char *morseTranslator::translate(const char text[], bool spacesBetweenLetters)
-{
+/**
+ * @brief Get the morse code of a char array
+ * 
+ * @param text : the char array to translate
+ * @param spacesBetweenLetters (optional) : bool value to add spaces between letters in the returned translation
+ * @return char* : the pointer to the translated char array, ends with '\0'
+ */
+char *morseTranslator::translate(const char text[], bool spacesBetweenLetters) {
   uint16_t lengthText = 0;
   while (text[lengthText] != '\0')
     lengthText++;
 
-  const char* m;
+  const char *m;
 
   int translation_index = 0;
   int morse_index = 0;
 
-  for (uint16_t text_index = 0; text_index < lengthText; text_index++)
-  {
+  for (uint16_t text_index = 0; text_index < lengthText; text_index++) {
     // strcpy(m, this->getMorse(text[text_index]));
     m = this->getMorse(text[text_index]);
 
     morse_index = 0;
-    while (*(m + morse_index) != '\0')
-    {
+    while (*(m + morse_index) != '\0') {
       this->translation[translation_index] = *(m + morse_index);
       translation_index++;
       morse_index++;
--- C++/MorseCode/arduino/main/morseTranslator.h
+++ C++/MorseCode/arduino/main/morseTranslator.h
@@ -3,39 +3,49 @@
 
 #include <stdint.h>
 
-class morseTranslator
-{
-  private:
+class morseTranslator {
+private:
+  static const uint8_t nbOfChars = 63;
+  const char characters[nbOfChars] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ' };
+  const char *morseCode[nbOfChars] = {
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+    ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", " "
+  };
 
-    static const uint8_t nbOfChars = 63;
-    const char characters[nbOfChars] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '
-                                       };
-    const char *morseCode[nbOfChars] = {
-      ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
-      ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
-      ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", " "
-    };
+  static const uint8_t morseLength = 6;
+  const char nullMorse[1] = { '\0' };
 
-    static const uint8_t morseLength = 6;
-    const char nullMorse[1] = {'\0'};
+  static const uint16_t translationLength = morseLength * 20;
+  char translation[translationLength];
 
-    static const uint16_t translationLength = morseLength * 20;
-    char translation[translationLength];
+  /**
+   * @brief Get the code morse of a single character
+   *
+   * @param c : the char to translate
+   * @return const char*
+   */
+  const char *getMorse(char c);
 
-    const char *getMorse(char c);
+public:
+  /**
+   * @brief Construct a new morse Translator::morse Translator object
+   */
+  morseTranslator();
+  /**
+   * @brief Destroy the morse Translator::morse Translator object
+   */
+  ~morseTranslator();
 
-  public:
-    morseTranslator();
-    ~morseTranslator();
-
-    /**
-       @brief translates text into morse code
-
-       @param text the sentence to translate
-       @return char* containing the char list representing morse
-    */
-    char *translate(const char text[], bool spacesBetweenLetters = true);
+  /**
+   * @brief Get the morse code of a char array
+   *
+   * @param text : the sentence [char array] to translate
+   * @param spacesBetweenLetters (optional) : bool value to add spaces between letters in the returned translation
+   * @return char* : the pointer to the translated char array, ends with '\0'
+   */
+  char *translate(const char text[], bool spacesBetweenLetters = true);
 };
 #endif
